a2e49fd748900703d832365b887f0e0339dea7fa
Mohit Arora <me@aboutmohit.com>
2024-03-15 18:03:51 -0400

    team update

--- src/components/Sections/OurTeam.tsx
+++ src/components/Sections/OurTeam.tsx
@@ -6,22 +6,19 @@ const ourTeam = {
     {
       name: "Sophia G. Contreras Stone",
       title: "Founder, CEO",
-      description:
-        "Award-winning entrepreneur dedicated to transforming the consultant marketplace.",
+      description: "Award-winning entrepreneur dedicated to transforming the consultant marketplace.",
       link: "https://www.linkedin.com/in/sophiagcstone/",
     },
     {
       name: "Mohit Arora",
       title: "CTO",
-      description:
-        "Experienced Software Architect passionate about innovative solutions and leadership.",
+      description: "Experienced Software Architect passionate about innovative solutions and leadership.",
       link: "https://www.linkedin.com/in/mohitaroralive/",
     },
     {
       name: "Asif Lakhany",
       title: "Head Quant",
-      description:
-        "Quantitative expert with a passion for risk management and technology.",
+      description: "Quantitative expert with a passion for risk management and technology.",
       link: "https://www.linkedin.com/in/asif-lakhany-8668b64/",
     },
   ],
@@ -29,45 +26,33 @@ const ourTeam = {
     {
       name: "Juan Pujadas",
       title: "Enterprise Risk and Professional Services Advisory",
-      description:
-        "Experienced board member and advisor in finance and technology.",
+      description: "Experienced board member and advisor in finance and technology.",
       link: "https://www.linkedin.com/in/juan-pujadas-13364487/",
     },
     {
       name: "Neil Bartlett",
       title: "Risk Data and Technology Advisory",
-      description:
-        "Veteran in the finance industry with a focus on data analytics and risk management.",
+      description: "Veteran in the finance industry with a focus on data analytics and risk management.",
       link: "https://www.linkedin.com/in/neiledwardbartlett/",
+    },
+    {
+      name: "Leslie J. Anderson-Rutland, CTP",
+      title: "Risk Data and Technology Advisory",
+      description: "Experienced Board Leader; Accomplished Finance, Treasury, Tech and Experience Leader.",
+      link: "https://www.linkedin.com/in/lesliejanderson/",
     },
     {
       name: "Eva Cruz, PhD, MBA, FRM",
       title: "Quantitative Risk Advisory",
-      description:
-        "Expert in quantitative techniques and risk management practices in banking.",
+      description: "Expert in quantitative techniques and risk management practices in banking.",
       link: "https://www.linkedin.com/in/evabcruz/",
     },
   ],
 };
 
-function TeamMember({
-  name,
-  title,
-  description,
-  link,
-}: {
-  name: string;
-  title: string;
-  description: string;
-  link: string;
-}) {
+function TeamMember({ name, title, description, link }: { name: string; title: string; description: string; link: string }) {
   return (
-    <a
-      className="flex flex-col border-l-2 pl-4 border-gray-200 hover:border-primary-main md:flex-row items-center justify-center md:justify-between mb-8"
-      target="_blank"
-      rel="noreferrer"
-      href={link}
-    >
+    <a className="flex flex-col border-l-2 pl-4 border-gray-200 hover:border-primary-main md:flex-row items-center justify-center md:justify-between mb-8" target="_blank" rel="noreferrer" href={link}>
       {/* <div className="md:w-1/3">
          <img
            src="sophia.jpg"
@@ -91,10 +76,8 @@ export function OurTeam() {
         <div className="">
           <SmallHeader>Meet The Team</SmallHeader>
           <p className="">
-            <strong>Our Mission:</strong> Empower enterprises to manage
-            consultants effortlessly, provide consulting firms with avenues for
-            growth and collaboration, and elevate the careers of independent
-            consultants through unparalleled opportunities.
+            <strong>Our Mission:</strong> Empower enterprises to manage consultants effortlessly, provide consulting firms with avenues for growth and collaboration, and elevate the careers of
+            independent consultants through unparalleled opportunities.
           </p>
           <hr className="bg-grey-400 mb-4 mt-10" />
         </div>
@@ -103,26 +86,14 @@ export function OurTeam() {
           {
             // Core Team
             ourTeam.coreTeam.map((teamMember, index) => (
-              <TeamMember
-                key={"coreTeam-" + index}
-                name={teamMember.name}
-                title={teamMember.title}
-                description={teamMember.description}
-                link={teamMember.link}
-              />
+              <TeamMember key={"coreTeam-" + index} name={teamMember.name} title={teamMember.title} description={teamMember.description} link={teamMember.link} />
             ))
           }
           <h2 className="text-2xl text-gray-400 pt-6 mb-4">Advisory Team</h2>
           {
             // Advisory Team
             ourTeam.advisoryTeam.map((teamMember, index) => (
-              <TeamMember
-                key={"advisoryTeam-" + index}
-                name={teamMember.name}
-                title={teamMember.title}
-                description={teamMember.description}
-                link={teamMember.link}
-              />
+              <TeamMember key={"advisoryTeam-" + index} name={teamMember.name} title={teamMember.title} description={teamMember.description} link={teamMember.link} />
             ))
           }
         </div>
